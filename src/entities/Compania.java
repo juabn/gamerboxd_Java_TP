070c6cd9a54b4a5dcbd0e3b439a64485fcfb1766
Santiago Malet <santiago.malet.12@gmail.com>
2026-04-21 16:24:50 -0300

    segundo commit juamba gay

--- src/entities/Compania.java
+++ src/entities/Compania.java
@@ -1,5 +1,31 @@
 package entities;
 
-public class Compania {
+class Compania {
+	
+	private int id;
+	private String nombre;
+	private String descripcion;
+	
+	
+	public int getId_comp() {
+		return id;
+	}
+	public void setId_comp(int id_comp) {
+		this.id = id_comp;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
 
+	
+	
 }
--- src/entities/Grupo.java
+++ src/entities/Grupo.java
@@ -1,5 +1,37 @@
 package entities;
 
-public class Grupo {
-
+ class Grupo {
+	 private int id;
+	 private String foto_perfil;
+	 private String nombre;
+	 private String descripcion;
+	 
+	 
+	 public int getId() {
+		 return id;
+	 }
+	 public void setId(int id) {
+		 this.id = id;
+	 }
+	 public String getFoto_perfil() {
+		 return foto_perfil;
+	 }
+	 public void setFoto_perfil(String foto_perfil) {
+		 this.foto_perfil = foto_perfil;
+	 }
+	 public String getNombre() {
+		 return nombre;
+	 }
+	 public void setNombre(String nombre) {
+		 this.nombre = nombre;
+	 }
+	 
+	 public String getDescripcion() {
+		 return descripcion;
+	 }
+	 public void setDescripcion(String descripcion) {
+		 this.descripcion = descripcion;
+	 }
+	 
+	 
 }
