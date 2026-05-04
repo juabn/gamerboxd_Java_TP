6848335c55709cd54f5cbe592f6c28d071e44e87
Santiago Malet <santiago.malet.12@gmail.com>
2026-05-04 08:14:12 -0300

    empiezo api persona

--- Backend/data/ApiPersona.java
+++ Backend/data/ApiPersona.java
@@ -1,5 +1,67 @@
 package data;
 
+import java.sql.*;
+
 public class ApiPersona {
+	public static void main(String[] args) {
+		
+		
+		obtener_todos();
+		
+		
+		
+	}
+	
+		private static void obtener_todos() {
+			
+			try {
+				
+				String url = "jdbc:mysql://localhost:3306/gamerboxd";
+				
+				String usuario = "root"; 
+			    String password = "12345";
+					
+				
+			    Connection conn = DriverManager.getConnection(url, usuario, password);
+				String query = "select * from persona";
+				PreparedStatement Resultado = conn.prepareStatement(query);
+				ResultSet rs = Resultado.executeQuery();
+				
+				
+				while (rs.next()) {
+					
+					String nombre = rs.getString("nombre");
+					
+					System.out.println(nombre);
+					
+				}
+				
+				
+				
+				
+				
+		
+			}
+			
+			
+			catch(SQLException ex){
+				
+				
+				System.out.println("SQLException: " + ex.getMessage());
+			    System.out.println("SQLState: " + ex.getSQLState());
+			    System.out.println("VendorError: " + ex.getErrorCode());
+			}
+			
+			
+			
+			
+		
+		
+		
+		
+		
+	
 
 }
+}
+
