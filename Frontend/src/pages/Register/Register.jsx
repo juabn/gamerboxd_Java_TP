2833d704504962bfc7f60ef1236a67b773e8414e
Santiago Malet <santiago.malet.12@gmail.com>
2026-05-04 17:13:00 -0300

    empiezo front register

---
+++ Frontend/src/pages/Register/Register.jsx
@@ -0,0 +1,28 @@
+
+import './Register.css'
+
+function Hola(){
+	
+	
+	return(
+		
+		<div className="contenedorPrincipal">
+		<div className="primerDiv">
+		
+		<p> primerdiv </p>
+		
+		</div>
+		
+		<div>
+		
+		<p> segundodiv </p>
+		
+		</div>
+		
+		
+		
+		</div>
+	);
+}
+
+export default Hola;
