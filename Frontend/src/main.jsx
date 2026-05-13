5733442c3514634c56f879feae702aedb8409bcc
Santiago Malet <santiago.malet.12@gmail.com>
2026-05-13 15:59:19 -0300

    commit para mergear

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
 import Login from './pages/login/login.jsx'
 import Registro from './pages/Register/Register.jsx'
 
