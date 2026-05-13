f3fb586995e9757c1a99685674cfd3951c4bfd2f
juabn <gaboteamodemasiado@gmail.com>
2026-05-13 10:40:32 -0300

    baby

--- Backend/data/DataJuego.java
+++ Backend/data/DataJuego.java
@@ -11,12 +11,12 @@ import entities.Plataforma;
 
 public class DataJuego {
 
-	
+	/*
 	public void guardarJuegoCompleto(Juego juego) {
-	    Connection conn = ConnectionManager.getConnection(); // Tu clase de conexión
+	   // Connection conn = ConnectionManager.getConnection(); 
 	    
 	    try {
-	        // 1. Persistir Plataformas y Compañías primero
+	        
 	        for (Plataforma p : juego.getPlataformas()) {
 	        	DataPlataforma dp = new DataPlataforma();
 	            dp.persistirPlataforma(p, conn);
@@ -26,19 +26,10 @@ public class DataJuego {
 	            dc.persistirCompania(c, conn);
 	        }
 
-	        String sqlJuego = "INSERT IGNORE INTO juego (idjuego, titulo, descripcion, imagen) VALUES (?, ?, ?, ?)";
-	        PreparedStatement stmtJuego = conn.prepareStatement(sqlJuego);
-	        stmtJuego.setInt(1, juego.getId_juego());
-	        stmtJuego.setString(2, juego.getTitulo());
-	        stmtJuego.setString(3, juego.getDescripcion());
-	        stmtJuego.setString(4, juego.getImagen());
-	        stmtJuego.executeUpdate();
 
-	        // 3. Persistir Relaciones en tablas intermedias
-	       // guardarRelaciones(juego, conn);
 
 	    } catch (SQLException e) {
 	        e.printStackTrace();
 	    }
-	}
+	}*/
 }
