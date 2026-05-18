7e9a6648ccd721e645487acabba06b35777c40d4
juabn <gaboteamodemasiado@gmail.com>
2026-05-18 18:44:17 -0300

    anda todo impecable

--- Frontend/src/components/carrousel/Carrousel.jsx
+++ Frontend/src/components/carrousel/Carrousel.jsx
@@ -1,16 +1,24 @@
 import { useState, useEffect } from 'react';
-import './carrousel.css';
+import './Carrousel.css';
 
 
 
 export default function Carrousel({ imagenes }) {
 	
   const [currentIndex, setCurrentIndex] = useState(0);
+  
+  const [animacionClase, setAnimacionClase] = useState(false);
+  
+  useEffect(()=>{
+	const animacionId = setInterval(()=> {
+		setAnimacionClase((prev)=> !prev);
+	}, 1500);
+	return() => {
+		clearInterval(animacionId)
+	}
+  }, []);
 
-  const TabContainer() => {
-	const [isPending, startTransition] = useTransition();
-	const [tab, setTab] = useState('about');
-  }
+
   const siguienteImagen = () => {
     setCurrentIndex((prevIndex) =>
       prevIndex === imagenes.length - 1 ? 0 : prevIndex + 1
@@ -18,7 +26,9 @@ export default function Carrousel({ imagenes }) {
   };
 	
   useEffect(() => {
-
+	
+	
+	
       const temporizador = setInterval(() => {
         siguienteImagen();
       }, 5000);
@@ -33,19 +43,8 @@ export default function Carrousel({ imagenes }) {
         <img 
           src={imagenes[currentIndex]} 
           alt={`Slide ${currentIndex + 1}`} 
-          className="carrousel-img" 
+          className={animacionClase ? "fade-out" : "fade-in"} 
         />
-      </div>
-
-
-
-      <div className="carrousel-dots">
-        {imagenes.map((_, index) => (
-          <span
-            key={index}
-            className={`dot ${index === currentIndex ? 'active' : ''}`}
-          />
-        ))}
       </div>
     </div>
   );
