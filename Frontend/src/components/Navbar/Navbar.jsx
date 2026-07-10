9ba3eb91509fcca5d7b4cc13b706b7b5d1b95474
Santiago Malet <santiago.malet.12@gmail.com>
2026-07-10 10:06:29 -0300

    fix token cuando no estas logeado y validacion de usuario o administrador

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -8,6 +8,32 @@ import { useState} from 'react';
 
 export default function Navbar({autenticado}) {
 	
+	const [imagen, setimagen] = useState("")
+	
+	
+	
+	const renderUsuarioLogueado = () => {
+	    return (
+	        <div className='usuariologeado'>
+	            <button className="btnlogin" onClick={handleLogout}>
+	                Cerrar Sesión
+	            </button>
+	            <div className="avatar-perfil" onClick={() => console.log("Ir al perfil")}>
+	                <img 
+	                    src={imagen || null} 
+	                    onClick={() => navigate('/Modificarperfil')} 
+	                    alt="Perfil" 
+	                    className="avatar-img" 
+	                />
+	            </div>
+	        </div>
+	    );
+	};
+	
+	
+	const [rol, setrol] = useState("");
+	
+	
 	const navigate = useNavigate();
 	
 	const handleLogout = () => {
@@ -18,8 +44,10 @@ export default function Navbar({autenticado}) {
 	  
 	  
 	  let tokenActual = localStorage.getItem('token');
+	 
 	  
-	  
+	  //trae rol de persona(administrador o usuario)
+	  if(tokenActual !== null){
 	  fetch('http://localhost:8081/verificarjwt',{
 		method: 'POST', 
 			  	  		  headers: {
@@ -31,13 +59,17 @@ export default function Navbar({autenticado}) {
 	  .then(response => response.json())
 	  .then(data => {
 	    console.log(data)
+		setrol(data)
 	  	  	  		   
   	  		})
 	.catch(error => console.error('Error:', error));
 	  
-	  const [imagen, setimagen] = useState("")
+	}else{
+		console.log("no estas logeado")
+	}
+	  
 	  	  
-	  	
+	if(tokenActual !== null){  	
 	  	  
 	  	  fetch('http://localhost:8081/fotousuario', {
 			
@@ -57,7 +89,7 @@ export default function Navbar({autenticado}) {
 	  	  		})
 	  	  		.catch(error => console.error('Error:', error));
 	
-
+	}
 
   return (
 		<nav>
@@ -77,21 +109,20 @@ export default function Navbar({autenticado}) {
 			</div> 
 			<div className="divlogin">
 			{autenticado ? (
-				<div className='usuariologeado'>
-				  <button className="btnlogin" onClick={handleLogout}>
-				    Cerrar Sesion
-				  </button>
-				  
-				  
-				  <div className="avatar-perfil" onClick={() => console.log("Ir al perfil")}>
-				    <img src={imagen} onClick={() => navigate('/Modificarperfil')} alt="Perfil" className="avatar-img" />
-				  </div>
-				</div>
-			        ) : (
-			          <Link to="/login">
-			            <button className="btnlogin">Login</button>
-			          </Link>
-			        )}
+				
+				rol == "usuario"? (
+					renderUsuarioLogueado()
+				):
+				(rol == "administrador"?(
+					renderUsuarioLogueado()):
+				(console.log("asd")))
+				
+				
+		        ) : (
+		          <Link to="/login">
+		            <button className="btnlogin">Login</button>
+		          </Link>
+		        )}
 			</div>
 		</nav>
   );
